a9a8ce644b6050f5bff15e873d0b7bd45be27b70
Mehdi Vasigh <mehdi.vasigh@gmail.com>
2021-02-07 16:21:04 -0500

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,103 @@
+use nannou::noise::NoiseFn;
+use nannou::{noise::Perlin, prelude::*};
+
+struct Line {
+    points: Vec<Point2<f64>>,
+}
+
+impl Line {
+    fn new(y: f64, width: f64) -> Line {
+        let _width = width.to_i32().unwrap();
+        let mut points = Vec::new();
+
+        for i in -(_width / 4)..(_width / 4) {
+            let x = i.to_f64().unwrap() * 4.0;
+            points.push(Point2::new(x, y))
+        }
+
+        Line { points }
+    }
+
+    fn update<F>(&mut self, func: F)
+    where
+        F: Fn(f64, f64) -> f64,
+    {
+        for point in self.points.iter_mut() {
+            point.y = func(point.x, point.y);
+        }
+    }
+
+    fn draw(&self, _app: &App, draw: &Draw, perlin: &Perlin, offset: f64) {
+        let points = self.points.iter().map(|point| {
+            let noise_val = perlin.get([point.x * 0.1, point.y * 0.1, offset]);
+            let alpha = map_range(noise_val, -1.0, 1.0, 0.0, 1.0);
+            let color = srgba(1.0, alpha.pow(2), alpha.pow(2), alpha.pow(3));
+
+            let y = point.y + (point.x + offset).sin();
+
+            (pt2(point.x as f32, (y+ noise_val) as f32), color)
+        });
+
+        draw.polyline()
+            .weight(2.0)
+            .join_round()
+            .points_colored(points);
+    }
+}
+
+struct Model {
+    _window: WindowId,
+    perlin: Perlin,
+    lines: Vec<Line>,
+    offset: f64,
+}
+
+fn main() {
+    nannou::app(model).update(update).run();
+}
+
+fn model(app: &App) -> Model {
+    let _window = app.new_window().size(800, 800).view(view).build().unwrap();
+    let perlin = Perlin::new();
+
+    let mut lines = Vec::new();
+    for i in -50..50 {
+        let y = i.to_f64().unwrap() * 8.0;
+        lines.push(Line::new(y, 800.0));
+    }
+
+    Model {
+        _window,
+        perlin,
+        lines,
+        offset: 0.0,
+    }
+}
+
+fn update(_app: &App, model: &mut Model, _update: Update) {
+    let offset = model.offset.to_owned();
+    // for line in model.lines.iter_mut() {
+    //     line.update(|x, y| {
+    //         let base = (x + offset).sin();
+
+    //         // println!("{}", base);
+    //         let new_val = y + base;
+
+    //         new_val
+    //     });
+    // }
+
+    model.offset += 0.01;
+}
+
+fn view(app: &App, model: &Model, frame: Frame) {
+    let draw = app.draw();
+
+    draw.background().color(BLACK);
+
+    for line in model.lines.iter() {
+        line.draw(&app, &draw, &model.perlin, model.offset.to_owned());
+    }
+
+    draw.to_frame(app, &frame).unwrap();
+}
